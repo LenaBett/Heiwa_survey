7b9d169561be670726322b444fd4217692d317da
LenaBett <rheabett@gmail.com>
2023-11-02 02:12:14 +0300

    Understanding servlets
    Implementing servlet via extending the GenericServlet class

---
+++ src/main/java/com/heiwa/servlet/Heiwa1.java
@@ -0,0 +1,16 @@
+package com.heiwa.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Heiwa1 extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        PrintWriter print = servletResponse.getWriter();
+        print.write("<b>Heiwa means peace<b/>");
+    }
+}
